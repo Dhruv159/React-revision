d76fc0cc78783dc3bfb2e71d73a01c944b744d88
Dhruv Sharma <dhruvsarkhandia9@gmail.com>
2026-08-10 20:36:06 +0530

    React setup

---
+++ React-prep/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Component1 from './components/text';
+
+const App = () => {
+  return (
+    <div>
+      Hi This is dhruv
+      <Component1/>
+    </div>
+  )
+}
+
+export default App;
---
+++ React-prep/src/components/text.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Component1 = () => {
+  return (
+    <div>
+      <h3>This is for testing purpose</h3>
+    </div>
+  )
+}
+
+export default Component1
