c2c3d1deacfa39642ea8251583be1ce62fb75a3e
Dhruv Sharma <dhruvsarkhandia9@gmail.com>
2026-08-13 23:26:00 +0530

    Notes app

---
+++ notes-app/src/App.jsx
@@ -0,0 +1,12 @@
+import React, { useState } from 'react'
+
+const App = () => {
+  const [userName, setUserName] = useState('')
+  return (
+    <div>
+      Hola
+    </div>
+  )
+}
+
+export default App
