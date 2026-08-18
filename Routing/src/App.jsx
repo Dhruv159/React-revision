8ff7eaa60f0a62a5f05209ddb722321ab4d2c60d
Dhruv Sharma <dhruvsarkhandia9@gmail.com>
2026-08-18 23:55:39 +0530

    react-router-dom

--- Routing/src/App.jsx
+++ Routing/src/App.jsx
@@ -1,10 +1,17 @@
-import React from 'react'
+import { Routes, Route } from "react-router-dom";
+
+import Home from './pages/Home'
+import Navbar from "./pages/components/Navbar";
 
 const App = () => {
   return (
-    <div>
-      
-    </div>
+    <>
+      <Navbar/>
+      <Routes>
+        <Route path="/" element={<Home/>}></Route>
+        <Route path="/about" element={<About/>}></Route>
+      </Routes>
+    </>
   )
 }
 
